3adac8eaf37497dca4cf7ad7ffedac6dc2d2c26d
Ayrton Day <AJ@notreal.com>
2025-03-13 19:17:29 -0700

    Chapter 4: Security - A Brief Introduction

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,3 +1,8 @@
 {
     'name': 'estate',
+    # data files are always loaded when the module is installed or updated and are loaded in the order they are listed
+    # in other words if data files have foreign key realationships, they should be in the order of dependencies
+    'data': [
+        'security/ir.model.access.csv',
+    ],
 }
