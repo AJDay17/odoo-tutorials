1e9c4475bee2f0e1e19168a52b4731dfdf9e017b
Ayrton Day <AJ@notreal.com>
2025-03-14 12:49:17 -0700

    Chapter 5: Finally, Some UI To Play With

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,10 @@
     'name': 'estate',
     # data files are always loaded when the module is installed or updated and are loaded in the order they are listed
     # in other words if data files have foreign key realationships, they should be in the order of dependencies
+    # menus and views should be loaded in order too, menu, action, view
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
     ],
 }
